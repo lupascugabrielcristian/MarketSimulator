357ae89f5a2db9b431cdc99380edcece1b4f8037
Cristi <lupascugabrielcristian@gmail.com>
2016-07-17 22:56:40 +0300

    Am lucrat la cargo operations interface

--- src/main/resources/static/javascript/controllers/cargoOperations.controller.js
+++ src/main/resources/static/javascript/controllers/cargoOperations.controller.js
@@ -3,4 +3,13 @@ angular.module('app').controller('cargoOperationsController', cargoOperationsCon
 function cargoOperationsController($scope, $stateParams, shipsManager, citiesManager, ngAudio) {
     $scope.ship = shipsManager.getShipById($stateParams.shipId);
     $scope.city = citiesManager.getCityById($stateParams.cityId);
+    $scope.valueOfselection = [];
+    var selectedCityCommodities = [];
+    var selectedShipsCommodities = [];
+
+    $scope.checkCargo = function(a, b){
+        console.log(a);
+        console.log(b);
+        console.log($scope.valueOfselection);
+    }
 }
