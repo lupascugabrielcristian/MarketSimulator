5e14ccc59a26c23bee4988e55aadc0d80a3c9407
Dan Misailescu <cristian.lupascu@jlg.ro>
2016-08-12 17:24:30 +0300

    Continuare modul fraction commodity

--- src/main/resources/static/javascript/directives/buyFraction.directive.js
+++ src/main/resources/static/javascript/directives/buyFraction.directive.js
@@ -10,8 +10,26 @@ function buyPartially(){
         },
         controller: function ($scope, events){
             $scope.id = "buy_partially_directive";
-            $scope.test = "OK";
             $scope.visibility = true;
+            $scope.ratio = 0;
+            $scope.necessaryVolume = 0;
+            $scope.necessaryMoney = 0;
+
+            $scope.increaseRatio = function() {
+
+            };
+
+            $scope.decreaseRatio = function() {
+
+            };
+
+            $scope.buy =function() {
+
+            };
+
+            $scope.cancel = function() {
+
+            };
 
             $scope.$on(events.modal, function(event, args){
                 if (args.id != $scope.id) return;
