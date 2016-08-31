9561a4aa70fc778ca654222fa8bc5ef9dd0a3fd2
Cristian Lupascu <cristian.lupascu@jlg.ro>
2016-08-31 19:45:16 +0300

    right panel options

--- src/main/resources/static/javascript/controllers/citiesView.controller.js
+++ src/main/resources/static/javascript/controllers/citiesView.controller.js
@@ -10,7 +10,65 @@ function citiesViewController($scope, citiesManager) {
         $scope.selectedCity = city
     };
 
-    $scope.editSelectedCity = function() {
+    $scope.options = [];
 
+    $scope.editSelectedCity = function() {
+        $scope.options.push(Options.FINISH);
     };
+
+    $scope.finishEdit = function() {
+        removeOption(Options.FINISH);
+    };
+
+    $scope.haveOption = function(optionName) {
+        return $scope.options.indexOf(optionName) != -1;
+    };
+
+    $scope.updateOptions = function() {
+        if ($scope.detailsVisible) {
+            addOption(Options.DETAILS);
+        }
+        else {
+            removeOption(Options.DETAILS);
+        }
+
+        if ($scope.factoriesVisible){
+            addOption(Options.FACTORIES);
+        }
+        else {
+            removeOption(Options.FACTORIES);
+        }
+
+        if ($scope.commoditiesVisible) {
+            addOption(Options.COMMODITIES);
+        }
+        else {
+            removeOption(Options.COMMODITIES);
+        }
+    };
+
+    function removeOption(optionName) {
+        var index = $scope.options.indexOf(optionName);
+        if (index != -1){
+            $scope.options.splice(index, 1);
+        }
+    }
+
+    function addOption(optionName) {
+        var index = $scope.options.indexOf(optionName);
+        if (index > -1){
+            return
+        }
+        else {
+            $scope.options.push(optionName);
+        }
+    }
+
+    function Options() {
+    }
+    Options.FINISH = "finish";
+    Options.DETAILS = "details";
+    Options.FACTORIES = "factories";
+    Options.COMMODITIES = "commodities";
+
 }
